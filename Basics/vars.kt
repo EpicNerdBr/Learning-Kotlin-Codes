af2e3e75e006b2448b5708a3e6f42a6a2072125a
EpicNerdBr <153876616+EpicNerdBr@users.noreply.github.com>
2024-12-20 01:57:46 -0300

    🌊 | Vars.kt (Variáveis)
    Exemplo de como declarar, atribuir valores e utilizar variáveis em Kotlin para armazenar dados e realizar operações

---
+++ Basics/vars.kt
@@ -0,0 +1,17 @@
+package org.EpicNerd
+
+fun main() {
+    // Definindo o número inicial de ingressos, baldes de pipoca e clientes
+    val totalTickets = 8
+    var totalBucketsOfPopcorn = 9
+    var totalCustomers = 7
+
+    // Exibindo a situação inicial
+    println("Existem $totalTickets ingressos, $totalBucketsOfPopcorn baldes de pipoca e $totalCustomers clientes na fila.")
+
+    // Um cliente desistiu
+    totalCustomers--  // Também poderíamos definir diretamente: totalCustomers = 6
+
+    // Exibindo a situação após a desistência
+    println("Após uma desistência, existem $totalTickets ingressos, $totalBucketsOfPopcorn baldes de pipoca e $totalCustomers clientes na fila.")
+}
